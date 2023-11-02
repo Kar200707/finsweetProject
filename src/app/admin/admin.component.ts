2efe82b8ab875dcb41ffbafbca9bc1ea86fe1f59
Kar200707 <harutyunyan2007karen@gmail.com>
2023-11-02 10:07:37 +0400

    fix buges

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -11,32 +11,6 @@ import {Authors} from "../models/authors";
 import {Router, RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {NgIf} from "@angular/common";
 
-interface FoodNode {
-  name: string;
-  children?: FoodNode[];
-}
-
-const TREE_DATA: FoodNode[] = [
-  {
-    name: 'Fruit',
-    children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
-  },
-  {
-    name: 'Vegetables',
-    children: [
-      {
-        name: 'Green',
-        children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
-      },
-      {
-        name: 'Orange',
-        children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
-      },
-    ],
-  },
-];
-
-
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
